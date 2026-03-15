426f6d86c3c39702d3e27fb5e884f6a983230cc6
malekkhaderr <malekkhader@outlook.com>
2026-03-15 06:23:15 +0300

    core: add dependency injection setup

---
+++ lib/core/di/injection_container.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+import '../network/dio_client.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Core
+  sl.registerLazySingleton<DioClient>(() => DioClient());
+}
